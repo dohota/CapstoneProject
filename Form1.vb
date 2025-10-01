894bece5d254d086c238529a3942ae6ce3bdf3e0
dohota <768913631@qq.com>
2025-10-01 19:39:35 +1000

    basic CRUD operations of database on VM

---
+++ Form1.vb
@@ -0,0 +1,162 @@
+﻿Imports Npgsql
+Public Class Form1
+    Private connString As String = "Host=130.33.112.233;Port=5432;Username=hj45h4gejgf;Password=s56jbjgf45j4gg054hcsfdsf;Database=jpdbrhtt"
+    '连接数据库服务器（静态IP地址），postgresql port is 5432
+    Private conn As NpgsqlConnection
+    Private WithEvents ButtonConnect As New Button()
+    Private WithEvents ButtonAdd As New Button()
+    Private WithEvents ButtonDelete As New Button()
+    Private WithEvents ButtonUpdate As New Button()
+    Private WithEvents ButtonSearch As New Button()
+    Private txtOutput As New TextBox()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ButtonConnect.Text = "Connect"
+        ButtonConnect.Location = New Point(30, 30)
+        Me.Controls.Add(ButtonConnect)
+    End Sub
+    Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
+        Try
+            conn = New NpgsqlConnection(connString)
+            conn.Open()
+            MessageBox.Show("Connected to database successfully!")
+
+            ' 添加 CRUD 按钮
+            ButtonAdd.Text = "Add"
+            ButtonAdd.Location = New Point(30, 70)
+            Me.Controls.Add(ButtonAdd)
+
+            ButtonDelete.Text = "Delete"
+            ButtonDelete.Location = New Point(30, 110)
+            Me.Controls.Add(ButtonDelete)
+
+            ButtonUpdate.Text = "Change"
+            ButtonUpdate.Location = New Point(30, 150)
+            Me.Controls.Add(ButtonUpdate)
+
+            ButtonSearch.Text = "Search"
+            ButtonSearch.Location = New Point(30, 190)
+            Me.Controls.Add(ButtonSearch)
+
+            txtOutput.Multiline = True
+            txtOutput.ScrollBars = ScrollBars.Vertical
+            txtOutput.Font = New Font("Consolas", 10)
+            txtOutput.Location = New Point(200, 30)
+            txtOutput.Size = New Size(400, 300)
+            Me.Controls.Add(txtOutput)
+
+        Catch ex As Exception
+            MessageBox.Show("Failed to connect: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonAdd_Click(sender As Object, e As EventArgs) Handles ButtonAdd.Click
+        Dim username As String = InputBox("Enter username:", "Add User")
+        Dim ageStr As String = InputBox("Enter email:", "Add User")
+        Dim email As Integer
+        If Not Integer.TryParse(ageStr, email) Then
+            MessageBox.Show("Invalid email input.")
+            Return
+        End If
+        Dim created As String = InputBox("Enter created_at:", "Add User")
+        Dim created_at As DateTime = DateTime.Parse(created)
+        Dim sql As String = "INSERT INTO users (username, email,created_at) VALUES (@username, @email,@created_at);"
+        Using cmd As New NpgsqlCommand(sql, conn)
+            cmd.Parameters.AddWithValue("username", username)
+            cmd.Parameters.AddWithValue("email", email)
+            cmd.Parameters.AddWithValue("created_at", created_at)
+            Try
+                Dim rows As Integer = cmd.ExecuteNonQuery()
+                MessageBox.Show($"{rows} row inserted。")
+            Catch ex As Exception
+                MessageBox.Show("WRONG：" & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
+        Dim username As String = InputBox("Enter name to delete:", "Delete User")
+        Dim sql As String = "DELETE FROM users WHERE username = @username;"
+        Using cmd As New NpgsqlCommand(sql, conn)
+            cmd.Parameters.AddWithValue("username", username)
+            Try
+                Dim rows As Integer = cmd.ExecuteNonQuery()
+                MessageBox.Show($"{rows} row(s) deleted.")
+            Catch ex As Exception
+                MessageBox.Show("WRONG：" & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub ButtonUpdate_Click(sender As Object, e As EventArgs) Handles ButtonUpdate.Click
+        Dim username As String = InputBox("Enter username to update:", "Update User")
+        Dim ageStr As String = InputBox("Enter new email:", "Update User")
+        Dim email As Integer
+        If Not Integer.TryParse(ageStr, email) Then
+            MessageBox.Show("Invalid email input.")
+            Return
+        End If
+        Dim sql As String = "UPDATE users SET email = @email WHERE username = @username;"
+        Using cmd As New NpgsqlCommand(sql, conn)
+            cmd.Parameters.AddWithValue("email", email)
+            cmd.Parameters.AddWithValue("username", username)
+            Try
+                Dim rows As Integer = cmd.ExecuteNonQuery()
+                MessageBox.Show($"{rows} row(s) updated.")
+            Catch ex As Exception
+                MessageBox.Show("WRONG：" & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    'Private Sub ButtonSearch_Click(sender As Object, e As EventArgs) Handles ButtonSearch.Click
+    '    Dim username As String = InputBox("Enter name to search:", "Search User")
+    '    Dim sql As String = "SELECT username, email FROM users WHERE username = @username;"
+    '    Using cmd As New NpgsqlCommand(sql, conn)
+    '        cmd.Parameters.AddWithValue("username", username)
+    '        Try
+    '            Using reader As NpgsqlDataReader = cmd.ExecuteReader()
+    '                If reader.Read() Then
+    '                    Dim foundName As String = reader.GetString(0)
+    '                    Dim foundEmail As Integer = reader.GetInt32(1)
+    '                    MessageBox.Show($"Found user: {foundName}, Email: {foundEmail}")
+    '                Else
+    '                    MessageBox.Show("No user found with that name.")
+    '                End If
+    '            End Using
+    '        Catch ex As Exception
+    '            MessageBox.Show("WRONG：" & ex.Message)
+    '        End Try
+    '    End Using
+    'End Sub
+    Private Sub ButtonShowTable_Click(sender As Object, e As EventArgs) Handles ButtonSearch.Click
+        Dim sql As String = "SELECT username, email, created_at FROM users;"
+        Dim output As New Text.StringBuilder()
+        Try
+            Using cmd As New NpgsqlCommand(sql, conn)
+                Using reader As NpgsqlDataReader = cmd.ExecuteReader()
+                    output.AppendLine(String.Format("{0,-5} {1,-15} {2,-5}", "ID", "Name", "Age"))
+                    output.AppendLine(New String("-"c, 30))
+
+                    While reader.Read()
+                        Dim username As String = reader.GetString(0)
+                        Dim email As String = reader.GetString(1)
+                        Dim created_at As DateTime = reader.GetDateTime(2)
+                        output.AppendLine(String.Format("{0,-5} {1,-15} {2,-5}", username, email, created_at))
+                    End While
+                End Using
+            End Using
+            txtOutput.Text = output.ToString()
+        Catch ex As Exception
+            MessageBox.Show("WRONG：" & ex.Message)
+        End Try
+    End Sub
+    ' 窗体关闭时释放数据库连接
+    Protected Overrides Sub OnFormClosing(e As FormClosingEventArgs)
+        MyBase.OnFormClosing(e)
+        If conn IsNot Nothing Then
+            conn.Close()
+            conn.Dispose()
+        End If
+    End Sub
+
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的一般信息由以下
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+'查看程序集特性的值
+
+<Assembly: AssemblyTitle("CloudatabaseTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CloudatabaseTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+<Assembly: Guid("1bb7e24f-e507-4bee-8126-65ddc853b705")>
+
+' 程序集的版本信息由下列四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
