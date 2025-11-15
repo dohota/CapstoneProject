ca60b969d2680a09157d7652e2d3aa6120f21e4d
dohota <768913631@qq.com>
2025-11-15 22:53:56 +1000

    v1.3

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import uvicorn as uvicorn
 from fastapi import FastAPI, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from starlette.websockets import WebSocket, WebSocketDisconnect
+
 from database import (
     init_db,
     create_item_db,
@@ -34,6 +37,7 @@ class Item(BaseModel):
     id: int
     name: str
     age: int
+
 
 @app.get("/favicon.ico")
 def get_favicon():
@@ -69,59 +73,78 @@ def read_index():
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
-
-# 创建数据库表
-Base.metadata.create_all(bind=engine)
+clients = []
 
 
-class PersonCreate(BaseModel):
-    name: str
-    age: int
-
-
-# 获取数据库连接
-def get_db():
-    db = SessionLocal()
+@app.websocket("/ws")
+async def websocket_endpoint(ws: WebSocket):
+    await ws.accept()
+    clients.append(ws)
     try:
-        yield db
-    finally:
-        db.close()
+        while True:
+            data = await ws.receive_text()
+            print("收到前端数据:", data)
+            # 可以广播给其他客户端
+            for client in clients:
+                if client != ws:
+                    await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(ws)
 
-
-@app.get("/intro")
-def read_index():
-    return FileResponse("static/personal.html")
-
-
-def get_persons(db: Session = Depends(get_db)):
-    return db.query(Person).all()
-
-# search
-#@app.get("/persons")
-
-
-@app.post("/intro")
-def create_person(person: PersonCreate, db: Session = Depends(get_db)):
-    db_person = Person(name=person.name, age=person.age)
-    db.add(db_person)
-    db.commit()
-    db.refresh(db_person)
-    return db_person
-
-
-@app.delete("/intro/{person_id}")
-def delete_person(person_id: int, db: Session = Depends(get_db)):
-    person = db.query(Person).filter(Person.id == person_id).first()
-    db.delete(person)
-    db.commit()
-    return {"message": "deleted"}
-
-
-@app.put("/intro/{person_id}")
-def update_person(person_id: int, person: PersonCreate, db: Session = Depends(get_db)):
-    db_person = db.query(Person).filter(Person.id == person_id).first()
-    db_person.name = person.name
-    db_person.age = person.age
-    db.commit()
-    return db_person
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
+# # 创建数据库表
+# Base.metadata.create_all(bind=engine)
+#
+#
+# class PersonCreate(BaseModel):
+#     name: str
+#     age: int
+#
+#
+# # 获取数据库连接
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+#
+#
+# @app.get("/intro")
+# def read_index():
+#     return FileResponse("static/personal.html")
+#
+#
+# def get_persons(db: Session = Depends(get_db)):
+#     return db.query(Person).all()
+#
+# # search
+# #@app.get("/persons")
+#
+#
+# @app.post("/intro")
+# def create_person(person: PersonCreate, db: Session = Depends(get_db)):
+#     db_person = Person(name=person.name, age=person.age)
+#     db.add(db_person)
+#     db.commit()
+#     db.refresh(db_person)
+#     return db_person
+#
+#
+# @app.delete("/intro/{person_id}")
+# def delete_person(person_id: int, db: Session = Depends(get_db)):
+#     person = db.query(Person).filter(Person.id == person_id).first()
+#     db.delete(person)
+#     db.commit()
+#     return {"message": "deleted"}
+#
+#
+# @app.put("/intro/{person_id}")
+# def update_person(person_id: int, person: PersonCreate, db: Session = Depends(get_db)):
+#     db_person = db.query(Person).filter(Person.id == person_id).first()
+#     db_person.name = person.name
+#     db_person.age = person.age
+#     db.commit()
+#     return db_person
 
