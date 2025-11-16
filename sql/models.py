7d9eccac2c377a210edff09158d84f08336f50d7
dohota <768913631@qq.com>
2025-11-16 17:54:23 +1000

    v1.5.c

--- sql/models.py
+++ sql/models.py
@@ -3,40 +3,50 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        self.db_path = "data.sqlite"# 数据库文件路径
         self.conn = None
         self.cursor = None
 
-    def connect(self) -> None:
-        self.conn = sqlite3.connect(self.db_path)
+    def database_init(self):
+        self.conn = sqlite3.connect("data.sqlite")  # 数据库文件路径
         self.conn.row_factory = sqlite3.Row  # 允许通过列名访问数据
         self.cursor = self.conn.cursor()
-        #return conn
 
-    def close(self) -> None:
-        if self.conn:
-            self.conn.close()
-
-    def create_user(self,name: str, email: str):
+    def create_field(self, name: str, age: int):
+        self.database_init()
         try:
             self.cursor.execute('''
-                INSERT INTO users (name, email) VALUES (?, ?)
-            ''', (name, email))
+                INSERT INTO items (name, age) VALUES (?, ?)
+            ''', (name, age))
             self.conn.commit()
         except sqlite3.IntegrityError:
             return None  # 如果电子邮件已存在
         finally:
             self.conn.close()
 
-    def get_user_by_id(self,user_id: int) -> dict:
-        self.cursor.execute('SELECT * FROM users WHERE id = ?', (user_id,))
-        user = self.cursor.fetchone()  # 获取一行数据
-        self.conn.close()
-        if user:
-            return dict(user)  # 将结果转换为字典格式
+    def delete_field(self):
+        self.database_init()
 
-    def get_all_users(self) -> list:
-        self.cursor.execute('SELECT * FROM users')
+    def update_field(self):
+        self.database_init()
+
+    def search_name(self, user_id: int) -> dict:
+        self.database_init()
+        # self.cursor.execute('SELECT * FROM name WHERE id = ?', (user_id,))
+        # user = self.cursor.fetchone()  # 获取一行数据
+        # self.conn.close()
+        # if user:
+        #     return dict(user)  # 将结果转换为字典格式
+
+    def search_all(self) -> list:
+        self.database_init()
+        self.cursor.execute('SELECT * FROM name')
         users = self.cursor.fetchall()  # 获取所有行数据
         self.conn.close()
         return [dict(user) for user in users]  # 返回字典列表
+
+
+if __name__ == "__main__":
+    d = Database()
+    d.create_field("Mary", 27)
+    d.create_field("Harry", 23)
+    d.create_field("Cindy Pay", 33)
